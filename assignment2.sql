056e24fd8b42113f1fce72c44c4598cefa6dae69
Julien Esposito <julien.esposito@gmail.com>
2016-11-03 17:28:35 -0700

    Session 2 - Part 1
    Simple Queries

--- assignment2.sql
+++ assignment2.sql
@@ -1,16 +1,25 @@
 -- drop table SALES_BRANCH
- create table SALES_BRANCH
-	(
-		BRANCH_NUM decimal (2,0), 
-		BRANCH_NAME char (50), 
-		BRANCH_LOCATION varchar (50),
-		NUM_EMPLOYEES decimal (2,0)
-		);
+--  create table SALES_BRANCH
+-- 	(
+-- 		BRANCH_NUM decimal (2,0), 
+-- 		BRANCH_NAME char (50), 
+-- 		BRANCH_LOCATION varchar (50),
+-- 		NUM_EMPLOYEES decimal (2,0)
+-- 		);
 
-describe SALES_BRANCH;
+-- describe SALES_BRANCH;
 
-insert into SALES_BRANCH values(5, 'Henry Town Plaza', '165 Plaza', 3 )
+-- insert into SALES_BRANCH values(5, 'Henry Town Plaza', '165 Plaza', 3 )
 
 -- Hey, please grade this ASAP
 
 -- OK - Hold on
+
+
+-- Now onto the rest of the lecture
+
+-- 01 - a general query on henry books
+-- select * from author; -- this pull all rows from the table author
+
+-- 02 - next example - only display first & last name.
+-- select author_first, author_last from author;
