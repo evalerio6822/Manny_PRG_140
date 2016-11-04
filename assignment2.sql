78e50deb8b412bcfc37a6dc4d331644825d693ed
Julien Esposito <julien.esposito@gmail.com>
2016-11-03 17:43:12 -0700

    Costraints
    Costraints

--- assignment2.sql
+++ assignment2.sql
@@ -23,3 +23,17 @@
 
 -- 02 - next example - only display first & last name.
 -- select author_first, author_last from author;
+
+-- 03 - Adding a constraint on the author number
+-- Here, we only want those authors for which the 
+-- author number is strictly less than 10
+-- select * from author where author_num < 10;
+
+-- 04 - Another example, but with a constraint on the 
+-- name
+-- select * from author where author_first like 'J%';
+
+-- 05 - Same as before, but with author_num strictly
+-- greater than 10
+select author_first, author_last from author where ( author_first like 'J%')
+ and ( author_num > 10 );
