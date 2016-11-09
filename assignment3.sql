575eab7fedd35241505c26edfc785e9c96f07a47
Enmanuel Valerio <enmanuel.valerio.f@gmail.com>
2016-11-09 10:54:07 -0800

    Assignment 3 finished minus question 9
    Assignment 3 finished minus question 9

--- assignment3.sql
+++ assignment3.sql
@@ -7,3 +7,8 @@
 -- question 7 select title from book where exists (select * from wrote, author where (book.book_code=wrote.book_code) and (wrote.author_num=author.author_num) and author.author_num in (18));
 -- question 8 select book.book_code, title from book, wrote, inventory where (book.book_code=wrote.book_code) and( book.book_code=inventory.book_code) and branch_num=2 and author_num=20;
 -- question 9 ? List the book code for each pair of books that have the same price. I can't figure out how to do this one. PLease Help.
+-- question 10 select title, author_last, on_hand from book, wrote, inventory, author where (book.book_code=inventory.book_code) and (book.book_code=wrote.book_code) and (wrote.author_num=author.author_num) and branch_num=4;
+-- qusestion 11 select title, author_last, on_hand from book, wrote, inventory, author where (book.book_code=inventory.book_code) and (book.book_code=wrote.book_code) and (wrote.author_num=author.author_num) and branch_num=4 and paperback='Y';
+-- question 12 select book_code, title from book, publisher where (book.publisher_code=publisher.publisher_code) and (price>10 or city='Boston');
+-- question 13 select book_code, title from book, publisher where (book.publisher_code=publisher.publisher_code) and price>10 and city='Boston';
+-- question 14 select book_code, title from book, publisher where (book.publisher_code=publisher.publisher_code) and price>10 and city not in (select city from publisher where city='Boston');
